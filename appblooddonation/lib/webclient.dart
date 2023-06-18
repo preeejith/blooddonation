8d383b24b090db3278acc1c3a378695ddf2514ff
preeejith <prejithts2021@it.ajce.in>
2023-06-18 09:51:15 +0530

    c

--- appblooddonation/lib/webclient.dart
+++ appblooddonation/lib/webclient.dart
@@ -9,8 +9,8 @@ class WebClient {
   // static const baseUrl = "http://192.168.54.155:5200";
 /////
   // static const baseUrl = "http://192.168.52.136:4000";
-  static const baseUrl = "https://api.gtschooloflogistics.com/";
-  static const imageUrl = "http://apicerp.leopardtechlabs.in/file/get/";
+  static const baseUrl = "https://apidfzbfzdbzbdbzf.gtschooloflogistics.com/";
+  static const imageUrl = "http://apicecdgfabgzbzgbzrp.leopardtechlabs.in/file/get/";
 
   static Future<dynamic> post(url, data) async {
     var token = await PrefManager.getToken();
